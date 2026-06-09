cfc84810cdfc4ebeae37827f61b53c9dac1bb7af
Anjali <sj5065070@gmail.com>
2026-06-09 08:25:44 +0530

    first commit

--- Day1/task.py
+++ Day1/task.py
@@ -4,16 +4,108 @@
 
 # print(f"Hi, my name is {Name} I am {Age} years old and I live in {city}.")
 
+#################################################################### 2 
+
+# a=12
+# b=2
+
+# print("addintion",a+b)
+# print("Subtraction",a-b)
+# print("mulply",a*b)
+# print("division",a/b)
 
 
-a=12;
-b=9393;
-print("sum of two number : " ,a+b)
+
+###################################################### 3
+# a=12;
+# b=9393;
+# print("sum of two number : " ,a+b)
 
 
 
-lenght =10 
-width = 20;
+# lenght =10 
+# width = 20;
 
-print("print",lenght*width)
+# print("print",lenght*width)
 
+####################################################### 4
+
+
+# c=float(input("enter a any number"))
+# fa = (9/5) * c + 32
+
+# print(fa)
+
+
+
+
+# a=5;
+# b=6;
+# c=b
+# b=a;
+# print(b)
+
+# print(c)
+
+
+# p=float(input("enter a p : "))
+# r=float(input("enter a r : "))
+# t=float(input("enter a t : "))
+
+# SI= p * r * t /100
+# print(SI2)
+
+
+
+
+
+
+
+
+
+# a = float(input("Enter number 1: "))
+# b = float(input("Enter number 2: "))
+# c = float(input("Enter number 3: "))
+# d = float(input("Enter number 4: "))
+# e = float(input("Enter number 5: "))
+
+# average = (a + b + c + d + e) / 5
+
+# print("Average is:", average)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a = int(input("Enter an integer: "))
+# b = float(input("Enter a float number: "))
+
+
+# a_float = float(a)
+# b_int = int(b)
+
+# print("Integer to float:", a_float)
+# print("Float to integer:", b_int)
+
+
+# print(type(a))
+# print(type(b))
+
+
+
+n = int(input("Enter a number: "))
+
+if n % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
